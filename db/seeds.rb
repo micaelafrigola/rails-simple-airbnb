f298de5941d7b8121adc5dc653fee610a2091f48
micaela frigola <mica_frigola@icloud.com>
2023-05-24 19:56:38 +0200

    simple airbnb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+10.times do
+  Flat.create(
+    name: Faker::Name.name,
+    address: Faker::Address.street_name,
+    description:Faker::Coffee.origin,
+    price_per_night: rand(50..150),
+    number_of_guests: rand(1..6),
+    image: "https://images.unsplash.com/photo-1570129477492-45c003edd2be?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80"
+  )
+end
